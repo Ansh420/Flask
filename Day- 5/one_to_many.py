392dc3a4ffa9217ccb16e683013a77c8679e4fee
Aanshu maurya <91012440+Ansh420@users.noreply.github.com>
2024-08-13 07:12:00 +0530

    Completed

---
+++ Day- 5/one_to_many.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app =Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ipl.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Team(db.Model):
+
+    __tablename__ = "teams"
+    id = db.Column(db.Integer, primary_key=True)
+    team = db.Column(db.String(50), nullable=False,unique=True)
+    state = db.Column(db.String(50), nullable=False)
+    members = db.relationship("player", backref="team")
+
+    def __repr__(self):
+        return f"Team('{self.team}', '{self.state}')"
+    
+class player(db.Model):
+
+    __tablename__ = "players"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    nationality = db.Column(db.String(50), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
+
+    def __repr__(self):
+        return f"player('{self.name}', '{self.nationality}')"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
